6a2db55167b5dadae38ca8c1f5b920a6e71cb2d9
alex <alex@e-cube.nl>
2025-07-08 12:30:17 +0200

    Get inserts working

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-mod modules;
 mod entity;
+mod modules;
 
+use crate::entity::chat;
 use crate::modules::Module;
+use log::{info, log, warn};
+use sea_orm::{
+    ActiveModelTrait, ColumnTrait, ConnectOptions, Database, DatabaseConnection, EntityTrait,
+    IntoActiveModel, QueryFilter, Set,
+};
 use teloxide::{prelude::*, utils::command::BotCommands};
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
-    log::info!("Starting command bot...");
-
+    info!("Starting Ingrid...");
     let bot = Bot::from_env();
 
     Command::repl(bot, answer).await;
@@ -28,6 +33,25 @@ enum Command {
 }
 
 async fn answer(bot: Bot, message: Message, command: Command) -> ResponseResult<()> {
+    warn!(
+        "Received from chat ID {} user id {} message: '{}'",
+        message.chat.id,
+        message
+            .from
+            .clone()
+            .and_then(|user| Some(user.id.to_string()))
+            .unwrap_or_default(),
+        message.text().unwrap_or_default(),
+    );
+
+    let db = Database::connect(ConnectOptions::new(
+        dotenv::var("DATABASE_URL").expect("DATABASE_URL environment variable not set."),
+    ))
+    .await
+    .expect("Could not connect to the database.");
+
+    let chat = message.get_or_create_chat(&db).await;
+
     let (module, input): (&dyn Module, String) = match command {
         Command::Help => (&modules::help::Help {}, "".to_string()),
         Command::Tolkien(input) => (&modules::tolkien::Tolkien {}, input),
@@ -37,4 +61,29 @@ async fn answer(bot: Bot, message: Message, command: Command) -> ResponseResult<
 
     bot.send_message(message.chat.id, response).await?;
     Ok(())
+}
+
+trait InteractWithDb {
+    async fn get_or_create_chat(&self, db: &DatabaseConnection) -> chat::ActiveModel;
+}
+
+impl InteractWithDb for Message {
+    async fn get_or_create_chat(&self, db: &DatabaseConnection) -> chat::ActiveModel {
+        match chat::Entity::find()
+            .filter(chat::Column::TelegramId.eq(self.chat.id.0))
+            .one(db)
+            .await
+            .expect("Could not query the database")
+        {
+            Some(chat) => chat.into_active_model(),
+            None => chat::ActiveModel {
+                telegram_id: Set(self.chat.id.0 as i32),
+                ..Default::default()
+            }
+            .insert(db)
+            .await
+            .expect("Could not insert chat")
+            .into(),
+        }
+    }
 }
